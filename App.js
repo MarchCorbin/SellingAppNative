68654e58cde08025d84dea938be6e39989d58367
Corbin March <marchcorbin@gmail.com>
2021-03-10 12:44:10 -0700

    Added basic photo selection functionality

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import * as ImagePicker from 'expo-image-picker'
+import * as Permissions from 'expo-permissions'
 import { StyleSheet, Text, StatusBar, Dimensions, TouchableHighlight, Platform, View, SafeAreaView, Image, Button, Alert, TouchableOpacity, TextInput, Switch } from 'react-native';
 
 import WelcomeScreen from './app/screens/WelcomeScreen';
@@ -19,6 +21,7 @@ import AppTextInput from './app/components/AppTextInput'
 import AppPicker from './app/components/AppPicker'
 import LoginScreen from './app/screens/LoginScreen'
 import ListingEditScreen from './app/screens/ListingEditScreen'
+import ImageInput from './app/components/ImageInput';
 
 
 
@@ -26,9 +29,33 @@ export default function App() {
 
 const [firstName, setFirstName] = useState('')  
 const [isNew, setIsNew] = useState(false)
+const [imageUri, setImageUri] = useState()
+
+const requestPermission = async() => {
+  const {granted} = await ImagePicker.requestCameraRollPermissionsAsync()
+  if(!granted) alert('You need to enable permission in order to access the library.')
+}
+
+useEffect(() => {
+  requestPermission()
+}, [])
+
+const selectImage = async() => {
+  try {
+    const result = await ImagePicker.launchImageLibraryAsync()
+    if(!result.cancelled)
+    setImageUri(result.uri)
+  } catch (error) {
+    console.log('Error reading image', error)
+  }
+}
 
   return (  
-<ListingEditScreen />  );
+<Screen>
+  <Button title='Select Image' onPress={selectImage} />
+  <Image source={{uri: imageUri}} style={{width:200, height:200}} />
+  <ImageInput imageUri={imageUri} />
+</Screen>  );
 }
 
 

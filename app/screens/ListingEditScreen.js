40d6964a421ff791b9feb63245312beef08d9dc4
Corbin March <marchcorbin@gmail.com>
2021-03-13 19:13:36 -0700

    Built custom hook for fast location request

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -1,7 +1,7 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import { StyleSheet } from 'react-native';
 import * as Yup from 'yup';
-import * as Location from 'expo-location';
+
 
 import AppForm from '../components/AppForm'
 import AppFormField from '../components/AppFormField'
@@ -10,6 +10,7 @@ import SubmitButton from '../components/SubmitButton'
 import Screen from  '../components/Screen'
 import CategoryPickerItem from '../components/CategoryPickerItem'
 import FormImagePicker from '../components/FormImagePicker';
+import useLocation from '../hooks/useLocation';
 
 const validationSchema = Yup.object().shape({
   title: Yup.string().required().min(1).label("Title"),
@@ -27,19 +28,7 @@ const categories = [
 
 function ListingEditScreen(props) {
 
-  const [location, setLocation] = useState()
-
-  const getLocation = async() => {
-    const {granted} = await Location.requestPermissionsAsync()
-    if(!granted) return
-    const {coords: {latitude, longitude}} = await Location.getLastKnownPositionAsync()
-    setLocation({latitude, longitude})
-    
-  }
-
-  useEffect(() => {
-    getLocation()
-  },[])
+const location = useLocation()
 
   return (
    <Screen>

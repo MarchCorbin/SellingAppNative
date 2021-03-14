6ce3cc4e07e4001888c434a8c69b08107e98b38b
Corbin March <marchcorbin@gmail.com>
2021-03-14 17:50:30 -0600

    Built out basic navigation functionality between test components

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react';
 import * as ImagePicker from 'expo-image-picker'
 import * as Permissions from 'expo-permissions'
 import { StyleSheet, Text, StatusBar, Dimensions, TouchableHighlight, Platform, View, SafeAreaView, Image, Button, Alert, TouchableOpacity, TextInput, Switch } from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack'
+import {NavigationContainer, useNavigation} from '@react-navigation/native'
 
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
@@ -24,13 +26,51 @@ import ListingEditScreen from './app/screens/ListingEditScreen'
 import ImageInput from './app/components/ImageInput';
 import ImageInputList from './app/components/ImageInputList';
 
+const Link = () => {
+  const navigation = useNavigation()
+ return( <Button
+  title='Click'
+  onPress={() => navigation.navigate('TweetDetails')} />
+ )
+}
 
+
+const Tweets = ({navigation}) => (
+  <Screen>
+    <Text>Tweets</Text>
+    <Button
+    title='View Tweets'
+    onPress={() => navigation.navigate('TweetDetails', {id: 1})}
+    />
+  </Screen>
+)
+
+const TweetDetails = ({route}) => (
+  <Screen>
+    <Text>Tweet Details {route.params.id}</Text>
+  </Screen>
+)
+
+const Stack = createStackNavigator()
+
+const StackNavigator = () => (
+  <Stack.Navigator screenOptions={{
+    headerStyle: {backgroundColor:'dodgerblue'},
+    headerTintColor: 'white'
+  }}>
+    <Stack.Screen name='Tweets' component={Tweets} />
+    <Stack.Screen name='TweetDetails' component={TweetDetails} options={{headerStyle: {backgroundColor:'tomato'},
+    headerTintColor: 'white'
+  }} />
+  </Stack.Navigator>
+)
 
 export default function App() {
-
-
   return (  
-<ListingEditScreen /> );
+<NavigationContainer>
+  <StackNavigator />
+</NavigationContainer>
+    );
 }
 
 

6204e6148a0b4008ec169bc421986eaa5e5aa60b
Corbin March <marchcorbin@gmail.com>
2021-03-29 17:11:32 -0600

    Refactored api calls to be more dynamic

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { View, StyleSheet, Image, TouchableWithoutFeedback } from 'react-native'
+import { View, StyleSheet, TouchableWithoutFeedback } from 'react-native'
+import {Image} from 'react-native-expo-image-cache';
+
 import colors from '../config/colors';
 import AppText from './AppText/AppText';
 
-function Card({title, subTitle, price, imageUrl, onPress}) {
+function Card({title, subTitle, price, imageUrl, onPress, thumbnailUrl}) {
   return (
     <TouchableWithoutFeedback onPress={onPress}>
       <View style={styles.card}>
-        <Image source={{ uri: imageUrl }} style={styles.image} />
+        <Image uri={imageUrl} tint='light' preview={{uri: thumbnailUrl}} style={styles.image} />
         <View style={styles.detailsContainer}>
           <AppText style={styles.title} numberOfLines={1}>{title}</AppText>
           <AppText style={styles.subTitle} numberOfLines={2}>{subTitle}</AppText>

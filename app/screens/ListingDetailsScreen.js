901acb9d8dd641bd9bb56d2097c147a86a486f59
Corbin March <marchcorbin@gmail.com>
2021-03-29 17:12:37 -0600

    Fixed presentation bug not showing detailscreen images

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { View, Image, StyleSheet } from 'react-native'
+import { View, StyleSheet } from 'react-native'
+import {Image} from 'react-native-expo-image-cache';
+
 import App from '../../App';
 import AppText from '../components/AppText/AppText';
 import colors from '../config/colors';
@@ -9,7 +11,7 @@ function ListingDetailsScreen({route}) {
   const listing = route.params
   return (
   <View>
-    <Image style={styles.image} source={listing.image} />
+    <Image style={styles.image} uri={listing.images[0].url} preview={{uri: listing.images[0].thumbnailUrl}} tint='light' />
     <View style={styles.detailsContainer}>
     <AppText style={styles.title}>{listing.title}</AppText>
     <AppText style={styles.price}>{listing.price}</AppText>

817b36bdcd0560f4c5402834aa06a5497346e156
Corbin March <marchcorbin@gmail.com>
2021-03-30 18:27:37 -0600

    Created loading overlay when user registers

--- app/components/ActivityIndicator.js
+++ app/components/ActivityIndicator.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import LottieView from 'lottie-react-native'
+import {View, StyleSheet} from 'react-native';
 
 function ActivityIndicator({visible = false}) {
   if(!visible) return null;
 
   return (
-    <LottieView 
-      source={require('../assets/animations/loader.json')} 
-      autoPlay 
-      loop 
-    />
+    <View style={styles.overlay} >
+      <LottieView 
+        source={require('../assets/animations/loader.json')} 
+        autoPlay 
+        loop 
+      />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  overlay: {
+    height: '100%',
+    width: '100%',
+    backgroundColor: 'white',
+    zIndex: 1,
+    opacity: 0.8,
+    position: 'absolute',
+  }
+})
 
 
 export default ActivityIndicator;
--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -20,26 +20,28 @@ useEffect(() => {
 },[])
 
   return (
-    <Screen style={styles.screen}>
-      {error && <>
-      <AppText>Could not retrieve listings.</AppText>
-      <Button title="Retry" onPress={loadListings} />
-      </>}
-        <ActivityIndicator visible={loading} />
-        <FlatList 
-        data={listings}
-        keyExtractor={(listing) => listing.id.toString()}
-        renderItem={({item}) => 
-          <Card 
-          title={item.title}
-          subTitle={"$" + item.price}
-          imageUrl={item.images[0].url}
-          onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
-          thumbnailUrl={item.images[0].thumbnailUrl}
+    <>
+      <ActivityIndicator visible={loading} />
+      <Screen style={styles.screen}>
+        {error && <>
+        <AppText>Could not retrieve listings.</AppText>
+        <Button title="Retry" onPress={loadListings} />
+        </>}
+          <FlatList 
+          data={listings}
+          keyExtractor={(listing) => listing.id.toString()}
+          renderItem={({item}) => 
+            <Card 
+            title={item.title}
+            subTitle={"$" + item.price}
+            imageUrl={item.images[0].url}
+            onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
+            thumbnailUrl={item.images[0].thumbnailUrl}
+            />
+        }
           />
-      }
-        />
-    </Screen>
+      </Screen>
+    </>
   );
 }
 
